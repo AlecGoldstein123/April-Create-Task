b9ba1b9630e028c88fc1e9313baa5faae6b1346c
AlecGoldstein123 <34690976+AlecGoldstein123@users.noreply.github.com>
2018-04-15 12:52:15 -0400

    Add files via upload

---
+++ SmallestValue.py
@@ -0,0 +1,22 @@
+#Write a function tht finds and displays the smallest number in a list
+
+section12A= [80,75,88,80,90,77,65]
+section12B= [80,75,88,80,90,77,65]
+section12C= [80,75,88,80,90,77,65]
+section12D= [80,75,88,80,90,77,65]
+
+
+def findMinVal(myList):
+    print("Original values: ", myList)
+    minVal=100
+    for i in range(0,len(myList)):
+        if myList[i] < minVal:
+            minVal = myList[i]
+    print("The smallest value in the list is:",minVal)
+
+
+findMinVal(section12A)
+findMinVal(section12B)
+findMinVal(section12C)
+findMinVal(section12D)
+
