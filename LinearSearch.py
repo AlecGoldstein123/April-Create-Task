42a025088bf16be43396f86fc2be1f07fde1331f
AlecGoldstein123 <34690976+AlecGoldstein123@users.noreply.github.com>
2018-04-18 14:13:55 -0400

    Add files via upload

---
+++ LinearSearch.py
@@ -0,0 +1,20 @@
+#Linear search algorithm
+'''
+n = 10
+tries = 0
+for i in range(n):
+    tries = tries+1
+    print(tries)
+for j in range(n):
+    tries=tries-1
+    print(tries)
+'''
+
+#Second Algorithm
+
+n = 40
+i = n
+
+while i>0:
+    i = i//2
+    print(i)
